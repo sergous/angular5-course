b1407d19a107531baf4d21d4a0f7f2603b659e2f
Sergey Smirnov <sergous@gmail.com>
2018-02-28 22:28:57 +0200

    Update product model.

--- src/app/filters/cart.filter.ts
+++ src/app/filters/cart.filter.ts
@@ -2,6 +2,6 @@ import { CartItem } from "../models/cart-item.model";
 import { Product } from "../models/product.model";
 
 export const CART_FILTER = {
-  PRODUCT_NAME_EQ: (product: Product) => (item: CartItem) => item.product.name === product.name,
-  PRODUCT_NAME_NOT_EQ: (product: Product) => (item: CartItem) => item.product.name !== product.name
+  PRODUCT_NAME_EQ: (product: Product) => (item: CartItem) => item.product.menuItem === product.menuItem,
+  PRODUCT_NAME_NOT_EQ: (product: Product) => (item: CartItem) => item.product.menuItem !== product.menuItem
 };
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -2,11 +2,9 @@ import { ProductCategory } from "../constants/product-category.enum";
 
 export class Product {
   constructor(
-    public name: string,
-    public description: string,
+    public menuItem: string,
     public price: number,
-    public category: ProductCategory,
-    public isAvailible: boolean,
+    public itemType: ProductCategory,
   ) {
   }
 }
