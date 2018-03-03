9e4e380cdceae8188a78917a74050420b475eb2c
Sergey Smirnov <sergous@gmail.com>
2018-03-04 01:09:02 +0200

    Add Material to Cart.

--- src/app/components/cart/cart.module.ts
+++ src/app/components/cart/cart.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 import { CartItemComponent } from './cart-item/cart-item.component';
 import { CartListComponent } from './cart-list/cart-list.component';
 import { CartService } from '../../services/cart.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   declarations: [
     CartListComponent,
-    CartItemComponent
+    CartItemComponent,
   ],
   providers: [CartService],
   exports: [
