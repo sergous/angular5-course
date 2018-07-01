3daf4f48afe750eee40046211ccb8961be7d3d81
Sergey Smirnov <sergous@gmail.com>
2018-07-01 01:56:17 -0700

    Refactor constants service to app config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,16 @@ import { Component, Inject, Optional, SkipSelf } from '@angular/core';
 import { ConstantsService } from './services/constants.service';
 import { APP_NAME, APP_VERSION } from './constants';
 import { BUILD_ID } from './factories';
-
-export const constantsService = new ConstantsService(APP_NAME, APP_VERSION);
+import { AppConfig, IAppConfig } from './app.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [
-    { provide: ConstantsService, useValue: constantsService }
-  ]
 })
 export class AppComponent {
-  title: string;
-  version: string;
-  build: string;
-
   constructor(
-    @Optional() public constantsService: ConstantsService,
-    @Inject(BUILD_ID) private buildId: string,
+    @Inject(AppConfig) public config: IAppConfig,
+    @Inject(BUILD_ID) public buildId: string
   ) {}
-
-  ngOnInit() {
-    const appInfo = this.constantsService.getAppInfo();
-    const { title, version } = appInfo;
-    this.title = title;
-    this.version = version;
-    this.build = this.buildId;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ProductsService } from './services/products.service';
 import { CartService } from './services/cart.service';
@@ -10,33 +9,23 @@ import { ProductsModule } from './components/products/products.module';
 import { LocalStorageService } from './services/local-storage.service';
 import { ConfigOptionsService } from './services/config-options.service';
 import { GeneratorFactory, BUILD_ID } from './factories';
-import { ConstantsService } from './services/constants.service';
+import { AppConfigProvider } from './app.config';
 import { APP_NAME, APP_VERSION } from './constants';
 import { ZoomDirective } from './directives/zoom.directive';
 import { OrderByPipe } from './pipes/order-by.pipe';
 
-export const constantsService = new ConstantsService(APP_NAME, APP_VERSION);
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    ZoomDirective,
-    OrderByPipe
-  ],
-  imports: [
-    BrowserModule,
-    CartModule,
-    ProductsModule
-  ],
+  declarations: [AppComponent, ZoomDirective, OrderByPipe],
+  imports: [BrowserModule, CartModule, ProductsModule],
   providers: [
     ProductsService,
     CartService,
     LocalStorageService,
     ConfigOptionsService,
     OrderByPipe,
-    {provide: BUILD_ID, useFactory: GeneratorFactory(14)},
-    { provide: ConstantsService, useValue: constantsService }
+    AppConfigProvider,
+    { provide: BUILD_ID, useFactory: GeneratorFactory(14) },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
